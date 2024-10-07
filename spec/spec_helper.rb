d16c38ad2db80489a2f1e7d0e61a77a1de553360
Ivan Vergés <ivan@pokecode.net>
2024-10-07 12:18:34 +0200

    fix workflow

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,15 +2,11 @@
 
 require "decidim/dev"
 
-require "simplecov"
-SimpleCov.start "rails"
-if ENV["CODECOV"]
-  require "codecov"
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
-
 ENV["ENGINE_ROOT"] = File.dirname(__dir__)
+ENV["NODE_ENV"] ||= "test"
 
 Decidim::Dev.dummy_app_path = File.expand_path(File.join(__dir__, "decidim_dummy_app"))
+
+require "decidim/dev/test/base_spec_helper"
 
 require "decidim/dev/test/base_spec_helper"
