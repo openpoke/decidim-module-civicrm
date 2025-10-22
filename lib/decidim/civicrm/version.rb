f6ea1cf45805c21293deda206dc13044e7d958fe
elviabth <ejbenedith@gmail.com>
2025-10-22 19:02:34 +0200

    update ruby version

--- lib/decidim/civicrm/version.rb
+++ lib/decidim/civicrm/version.rb
@@ -3,8 +3,8 @@
 module Decidim
   # This holds the decidim-civicrm version.
   module Civicrm
-    DECIDIM_VERSION = "~> 0.29.3"
-    COMPAT_DECIDIM_VERSION = [">= 0.29", "< 0.30"].freeze
+    DECIDIM_VERSION = "~> 0.30.0"
+    COMPAT_DECIDIM_VERSION = [">= 0.30", "< 0.31"].freeze
     VERSION = "0.8.1"
   end
 end
