8a0af77f30a61ef7a08bfa0d74a3f7371e835ef4
Ivan Vergés <ivan@pokecode.net>
2026-02-13 13:43:20 +0100

    allow mixed types in membership search

--- app/queries/decidim/civicrm/find_group_member_by_custom_fields.rb
+++ app/queries/decidim/civicrm/find_group_member_by_custom_fields.rb
@@ -3,7 +3,7 @@
 module Decidim
   module Civicrm
     # Finds group memberships by matching custom field values within a specific group.
-    # Uses PostgreSQL JSONB containment operator (@>) for lookup.
+    # Checks if all provided field values match within the stored custom_fields JSONB.
     #
     # Returns an ActiveRecord relation of matching GroupMembership records.
     class FindGroupMemberByCustomFields < Decidim::Query
@@ -15,7 +15,12 @@ module Decidim
       def query
         return GroupMembership.none if @group.blank? || @fields.blank?
 
-        @group.group_memberships.where("custom_fields @> ?", @fields.to_json)
+        # Build conditions to check if each field key-value pair exists in custom_fields JSONB
+        conditions = @fields.map do |key, value|
+          "custom_fields ->> ? = ?"
+        end.join(" AND ")
+
+        @group.group_memberships.where(conditions, *@fields.flat_map { |k, v| [k, v] })
       end
     end
   end
